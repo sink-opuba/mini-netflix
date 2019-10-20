df49e0a1d6409c5bf033b5a4968a5f719cc84d58
sink-opuba <osinkopere@yahoo.com>
2019-10-20 09:31:55 +0100

    set db and create movie model

---
+++ src/app/movies/shared/movie.model.ts
@@ -0,0 +1,18 @@
+export interface IMovie {
+  id: number;
+  title: string;
+  year: string;
+  rated: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  plot: string;
+  language: string;
+  country: string;
+  awards: string;
+  poster: string;
+  production: string;
+}
